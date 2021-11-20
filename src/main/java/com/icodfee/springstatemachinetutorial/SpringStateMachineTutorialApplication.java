961217f04089cedf7bfcd9d8199dae720d90872d
Andy Deng <52codfee@gmail.com>
2021-11-20 00:56:38 -0600

    Init Commit

---
+++ src/main/java/com/icodfee/springstatemachinetutorial/SpringStateMachineTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.icodfee.springstatemachinetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStateMachineTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStateMachineTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icodfee/springstatemachinetutorial/SpringStateMachineTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodfee.springstatemachinetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
